ce342becdaa10c059d90d1e04a5ca5e7e6dec866
KimeraRoyal <royalkimera01@gmail.com>
2022-02-25 17:25:15 +0000

    Created stat block class.

---
+++ Assets/Scripts/Battle/StatBlock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+namespace GSP.Battle
+{
+    [Serializable]
+    public struct StatBlock
+    {
+        /// <summary>
+        /// The character's maximum health.
+        /// </summary>
+        [SerializeField] private int m_HP;
+
+        /// <summary>
+        /// The character's maximum health.
+        /// </summary>
+        public int HP => m_HP;
+    }
+}
