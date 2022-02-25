1db70189ef8f33d82db388b052aea2b91db1a44b
KimeraRoyal <royalkimera01@gmail.com>
2022-02-25 17:25:32 +0000

    Added stat block to character.

--- Assets/Scripts/Battle/Character.cs
+++ Assets/Scripts/Battle/Character.cs
@@ -11,6 +11,11 @@ namespace GSP.Battle
         [SerializeField] private string m_name;
 
         /// <summary>
+        /// The character's stat values.
+        /// </summary>
+        private StatBlock m_statBlock;
+
+        /// <summary>
         /// All moves the character can select in the menu.
         /// </summary>
         [SerializeField] private List<Move> m_moveset;
@@ -19,6 +24,11 @@ namespace GSP.Battle
         /// The character's name.
         /// </summary>
         public string Name => m_name;
+
+        /// <summary>
+        /// The character's stat values.
+        /// </summary>
+        public StatBlock StatBlock => m_statBlock;
 
         /// <summary>
         /// All moves the character can select in the menu.
