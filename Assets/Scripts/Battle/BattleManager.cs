783365c4307d3412e9b754394e02ec682f519e66
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-02-26 14:38:31 +0000

    Added test battle manager script

---
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GSP.Battle.Party;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    [SerializeField] private EnemyPartyObject m_enemyParty;
+
+    private void Start()
+    {
+        var party = m_enemyParty.GetParty();
+        foreach (var partyMember in party.PartyMembers)
+        {
+            Debug.Log(partyMember.BaseCharacter.Name);
+        }
+    }
+}
