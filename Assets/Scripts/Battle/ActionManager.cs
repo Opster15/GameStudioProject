7c32d4bc25d39e32fa2c82e2232e340c12b3a523
KimeraRoyal <royalkimera01@gmail.com>
2022-02-28 17:46:52 +0000

    Added action manager.

---
+++ Assets/Scripts/Battle/ActionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GSP.Battle
+{
+    public class ActionManager : MonoBehaviour
+    {
+        private List<Action> m_actions;
+
+        private void Awake()
+        {
+            m_actions = new List<Action>();
+        }
+
+        public void QueueAction(Action _action)
+            => m_actions.Add(_action);
+
+        public void ExecuteActions()
+        {
+            SortQueue();
+
+            foreach(var action in m_actions)
+            {
+                Debug.Log("Action " + action.Move.Name + " Speed: " + action.Speed);
+            }
+            m_actions.Clear();
+        }
+
+        private void SortQueue()
+            => m_actions = m_actions.OrderBy(p => p.Speed).ToList();
+    }
+}
--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -1,53 +1,67 @@
 ﻿using System.Collections;
 using GSP.Battle.Party;
 using UnityEngine;
-
-public class BattleManager : MonoBehaviour
+namespace GSP.Battle
 {
-    private GameParty[] m_parties;
-
-    //TODO: Create battle
-    [SerializeField] private PartyObject m_A;
-    [SerializeField] private PartyObject m_B;
-
-    private void Awake()
+    public class BattleManager : MonoBehaviour
     {
-        m_parties = new GameParty[2];
-    }
+        private ActionManager m_actionManager;
 
-    //TODO: Create battle
-    private void Start()
-    {
-        StartBattle(m_A.GetParty(), m_B.GetParty());
-    }
+        private GameParty[] m_parties;
 
-    public void StartBattle(GameParty _a, GameParty _b)
-    {
-        m_parties[0] = _a;
-        m_parties[1] = _b;
+        //TODO: Create battle
+        [SerializeField] private PartyObject m_A;
+        [SerializeField] private PartyObject m_B;
 
-        StartCoroutine(Turn());
-    }
-
-    private IEnumerator Turn()
-    {
-        StartTurn();
-        for (var i = 0; i < m_parties.Length; i++) { yield return ChooseMoves(m_parties[m_parties.Length - i - 1], m_parties[i]); }
-    }
-
-    private void StartTurn()
-    {
-        
-    }
-
-    private IEnumerator ChooseMoves(GameParty _party, GameParty _opposingParty)
-    {
-        Debug.Log("Party Moves Start");
-        _party.BattleController.SetOpposingParty(_opposingParty);
-        for (var i = 0; i < _party.PartyMembers.Count; i++)
+        private void Awake()
         {
-            _party.BattleController.SelectPartyMember(i);
-            yield return new WaitUntil(() => _party.BattleController.GetChosenAction() != null);
+            m_actionManager = FindObjectOfType<ActionManager>();
+
+            m_parties = new GameParty[2];
+        }
+
+        //TODO: Create battle
+        private void Start()
+        {
+            StartBattle(m_A.GetParty(), m_B.GetParty());
+        }
+
+        public void StartBattle(GameParty _a, GameParty _b)
+        {
+            m_parties[0] = _a;
+            m_parties[1] = _b;
+
+            StartCoroutine(Turn());
+        }
+
+        private IEnumerator Turn()
+        {
+            StartTurn();
+            for (var i = 0; i < m_parties.Length; i++) { yield return ChooseMoves(m_parties[m_parties.Length - i - 1], m_parties[i]); }
+            m_actionManager.ExecuteActions();
+            EndTurn();
+        }
+
+        private void StartTurn()
+        {
+
+        }
+
+        private void EndTurn()
+        {
+            StartCoroutine(Turn());
+        }
+
+        private IEnumerator ChooseMoves(GameParty _party, GameParty _opposingParty)
+        {
+            Debug.Log("Party Moves Start");
+            _party.BattleController.SetOpposingParty(_opposingParty);
+            for (var i = 0; i < _party.PartyMembers.Count; i++)
+            {
+                _party.BattleController.SelectPartyMember(i);
+                yield return new WaitUntil(() => _party.BattleController.GetChosenAction() != null);
+                m_actionManager.QueueAction(_party.BattleController.GetChosenAction());
+            }
         }
     }
 }
