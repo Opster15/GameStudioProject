b4630a8c81106995c966a581381b05c61de97c18
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-02-28 11:16:05 +0000

    Testing basics of choosing moves in battle manager.

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -1,19 +1,50 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using GSP.Battle.Party;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    [SerializeField] private EnemyPartyObject m_enemyParty;
+    private GameParty[] m_parties;
+
+    private void Awake()
+    {
+        m_parties = new GameParty[2];
+    }
+
+    [SerializeField] private EnemyPartyObject m_A;
+    [SerializeField] private EnemyPartyObject m_B;
 
     private void Start()
     {
-        var party = m_enemyParty.GetParty();
-        foreach (var partyMember in party.PartyMembers)
+        StartBattle(m_A.GetParty(), m_B.GetParty());
+    }
+
+    public void StartBattle(GameParty _a, GameParty _b)
+    {
+        m_parties[0] = _a;
+        m_parties[1] = _b;
+
+        StartCoroutine(Turn());
+    }
+
+    private IEnumerator Turn()
+    {
+        StartTurn();
+        for (var i = 0; i < m_parties.Length; i++) { yield return ChooseMoves(m_parties[m_parties.Length - i - 1]); }
+    }
+
+    private void StartTurn()
+    {
+        
+    }
+
+    private IEnumerator ChooseMoves(GameParty _party)
+    {
+        for (var i = 0; i < _party.PartyMembers.Count; i++)
         {
-            Debug.Log(partyMember.BaseCharacter.Name);
+            _party.BattleController.SelectPartyMember(i);
+            yield return new WaitUntil(() => _party.BattleController.IsMoveChosen());
+            Debug.Log(_party.BattleController.GetChosenMove().Name);
         }
     }
 }
