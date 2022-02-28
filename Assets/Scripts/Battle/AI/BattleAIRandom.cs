4ea88b7d9cce5feafdab28ab3f1a8a2f48fc6a84
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-02-28 12:15:49 +0000

    Added random selection battle AI

---
+++ Assets/Scripts/Battle/AI/BattleAIRandom.cs
@@ -0,0 +1,19 @@
+﻿using GSP.Battle.Party;
+using UnityEngine;
+namespace GSP.Battle.AI
+{
+    [CreateAssetMenu(fileName = "Random", menuName = "GSP/AI/Battle AI Random")]
+    public class BattleAIRandom : BattleAIBase
+    {
+        public override Action SelectAction(int _characterID, GameParty _party, GameParty _opposingParty)
+        {
+            var character = _party.PartyMembers[_characterID];
+            
+            // Choose a random move and a random enemy to target.
+            var move = character.Moveset[Random.Range(0, character.Moveset.Count)];
+            var target = _opposingParty.PartyMembers[Random.Range(0, _opposingParty.PartyMembers.Count)];
+
+            return new Action(move, target);
+        }
+    }
+}
