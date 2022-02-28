6fc0e72eee61c558fc60712545081970a87e76c8
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-02-28 12:14:20 +0000

    Created base battle AI class.

---
+++ Assets/Scripts/Battle/AI/BattleAIBase.cs
@@ -0,0 +1,9 @@
+﻿using GSP.Battle.Party;
+using UnityEngine;
+namespace GSP.Battle.AI
+{
+    public abstract class BattleAIBase : ScriptableObject
+    {
+        public abstract Action SelectAction(int _characterID, GameParty _party, GameParty _opposingParty);
+    }
+}
