9f0b39d70f5aea5e1dbf88c0c2325abed4e15d98
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-02-28 11:03:40 +0000

    Added battle controller base class.

---
+++ Assets/Scripts/Battle/Controller/EnemyBattleController.cs
@@ -0,0 +1,26 @@
+﻿using GSP.Battle.Party;
+namespace GSP.Battle.Controller
+{
+    public class EnemyBattleController : IBattleController
+    {
+        private GameParty m_party;
+
+        private Move m_selectedMove;
+
+        public void SetParty(GameParty _party)
+        {
+            m_party = _party;
+        }
+
+        public void SelectPartyMember(int _partyMember)
+        {
+            m_selectedMove = m_party.PartyMembers[_partyMember].BaseCharacter.Moveset[0];
+        }
+
+        public bool IsMoveChosen()
+            => m_selectedMove != null;
+
+        public Move GetChosenMove()
+            => m_selectedMove;
+    }
+}
---
+++ Assets/Scripts/Battle/Controller/IBattleController.cs
@@ -0,0 +1,33 @@
+﻿using GSP.Battle.Party;
+namespace GSP.Battle.Controller
+{
+    /// <summary>
+    /// A framework for methods of controlling which moves party members select in battle.
+    /// </summary>
+    public interface IBattleController
+    {
+        /// <summary>
+        /// Set the party to control the actions of.
+        /// </summary>
+        /// <param name="_party">The party to control the actions of.</param>
+        void SetParty(GameParty _party);
+        
+        /// <summary>
+        /// Choose a party member to select the actions for.
+        /// </summary>
+        /// <param name="_partyMember">The index of the party member.</param>
+        void SelectPartyMember(int _partyMember);
+        
+        /// <summary>
+        /// Returns whether a move has been selected for the current party member.
+        /// </summary>
+        /// <returns>Whether a move has been selected.</returns>
+        bool IsMoveChosen();
+
+        /// <summary>
+        /// Return the selected move.
+        /// </summary>
+        /// <returns>The selected move.</returns>
+        Move GetChosenMove();
+    }
+}
