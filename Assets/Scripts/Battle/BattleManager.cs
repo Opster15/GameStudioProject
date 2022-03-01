a31a477b075a98d5546334a80b545db1410f3a69
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-03-01 18:55:45 +0000

    Oops I broke everything! Fixed now :-)

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -4,65 +4,25 @@ using GSP.LUA;
 using UnityEngine;
 namespace GSP.Battle
 {
-    private ScriptManager m_scriptManager;
-    
-    private GameParty[] m_parties;
-
-    private void Awake()
     public class BattleManager : MonoBehaviour
     {
-        m_scriptManager = FindObjectOfType<ScriptManager>();
-        
-        m_parties = new GameParty[2];
-    }
         private ActionManager m_actionManager;
-
+        private ScriptManager m_scriptManager;
+    
         private GameParty[] m_parties;
-
-        //TODO: Create battle
-        [SerializeField] private PartyObject m_A;
-        [SerializeField] private PartyObject m_B;
 
         private void Awake()
         {
             m_actionManager = FindObjectOfType<ActionManager>();
-
-        foreach(var party in m_parties)
-        {
-            foreach (var character in party.PartyMembers)
-            {
-                Debug.Log(character.Moveset[0].Script);
-                character.EnableScripts(m_scriptManager);
-                Debug.Log(character.Moveset[0].Script);
-            }
-        }
-
-        StartCoroutine(Turn());
-    }
+            m_scriptManager = FindObjectOfType<ScriptManager>();
+            
             m_parties = new GameParty[2];
         }
 
-    private void EndBattle()
-    {
-        foreach(var party in m_parties)
-        {
-            foreach (var character in party.PartyMembers)
-            {
-                Debug.Log(character.Moveset[0].Script);
-                character.DisableScripts(m_scriptManager);
-                Debug.Log(character.Moveset[0].Script);
-            }
-        }
-    }
-
-    private IEnumerator Turn()
-    {
-        StartTurn();
-        for (var i = 0; i < m_parties.Length; i++)
-        {
-            yield return ChooseMoves(m_parties[m_parties.Length - i - 1], m_parties[i]);
-        }
-    }
+        //TODO: Create battle
+        [SerializeField] private PartyObject m_A;
+        [SerializeField] private PartyObject m_B;
+        
         //TODO: Create battle
         private void Start()
         {
@@ -74,13 +34,39 @@ namespace GSP.Battle
             m_parties[0] = _a;
             m_parties[1] = _b;
 
+             foreach(var party in m_parties)
+            {
+                foreach (var character in party.PartyMembers)
+                {
+                    Debug.Log(character.Moveset[0].Script);
+                    character.EnableScripts(m_scriptManager);
+                    Debug.Log(character.Moveset[0].Script);
+                }
+            }
+
             StartCoroutine(Turn());
+        }
+
+        private void EndBattle()
+        {
+            foreach(var party in m_parties)
+            {
+                foreach (var character in party.PartyMembers)
+                {
+                    Debug.Log(character.Moveset[0].Script);
+                    character.DisableScripts(m_scriptManager);
+                    Debug.Log(character.Moveset[0].Script);
+                }
+            }
         }
 
         private IEnumerator Turn()
         {
             StartTurn();
-            for (var i = 0; i < m_parties.Length; i++) { yield return ChooseMoves(m_parties[m_parties.Length - i - 1], m_parties[i]); }
+            for (var i = 0; i < m_parties.Length; i++)
+            {
+                yield return ChooseMoves(m_parties[m_parties.Length - i - 1], m_parties[i]);
+            }
             m_actionManager.ExecuteActions();
             EndTurn();
         }
