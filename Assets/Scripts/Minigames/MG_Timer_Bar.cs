5e37a566ff1e0a8e315e8b38f1d914403b1a747f
Opster15 <60069488+Opster15@users.noreply.github.com>
2022-03-05 17:25:16 +0000

    Added Timer Visual. Fixed minigame bugs

---
+++ Assets/Scripts/Minigames/MG_Timer_Bar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GSP.Minigames
+{
+    public class MG_Timer_Bar : MonoBehaviour
+    {
+        public GameObject m_fill;
+        SpriteRenderer m_fillSprite;
+
+        public Gradient m_gradient;
+        Minigame minigame;
+
+
+        private void Start()
+        {
+            minigame = GetComponentInParent<Minigame>();
+            m_fillSprite = m_fill.GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            m_fillSprite.color = m_gradient.Evaluate(minigame.TimeTaken);
+            m_fill.transform.localScale = new Vector3(1 - minigame.TimeTaken, 1, 1);
+        }
+    }
+}
