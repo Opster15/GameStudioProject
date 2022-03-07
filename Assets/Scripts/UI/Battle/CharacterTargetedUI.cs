85136184a96788356cf34ed6f082b5f3e39caeda
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-03-07 08:57:07 +0000

    Refactored player health bar script.

---
+++ Assets/Scripts/UI/Battle/CharacterTargetedUI.cs
@@ -0,0 +1,22 @@
+﻿using GSP.Battle;
+using UnityEngine;
+namespace GSP.UI.Battle
+{
+    public class CharacterTargetedUI : MonoBehaviour
+    {
+        /// <summary>
+        /// The character to represent the health of.
+        /// </summary>
+        protected GameCharacter m_target;
+
+        /// <summary>
+        /// Set the slider's target character.
+        /// </summary>
+        /// <param name="_target">The character to represent the health of.</param>
+        public virtual void SetTarget(GameCharacter _target)
+        {
+            m_target = _target;
+            if (m_target == null) { return; }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Battle/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using GSP.Battle;
+using UnityEngine;
+using UnityEngine.UI;
+namespace GSP.UI.Battle
+{
+    public class HealthBar : CharacterTargetedUI
+    {
+        private Slider m_slider;
+
+        private void Awake()
+        {
+            m_slider = GetComponentInChildren<Slider>();
+        }
+
+        public override void SetTarget(GameCharacter _target)
+        {
+            base.SetTarget(_target);
+            
+            m_slider.maxValue = m_target.MaxHP;
+            m_slider.value = m_target.CurrentHP;
+        }
+
+        private void Update()
+        {
+            if (m_target == null) { return; }
+            m_slider.value = Mathf.Lerp(m_slider.value, m_target.CurrentHP, Time.deltaTime * 10f);
+        }
+    }
+}
