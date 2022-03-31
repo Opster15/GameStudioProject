0629a25d490dbfb3317386623a05e4b64e02fda5
Kimera Royal <royalkimera01@gmail.com>
2022-03-31 15:49:33 +0100

    Added eye twitch animation

--- Assets/Data/LUA/Move/move_guard.lua
+++ Assets/Data/LUA/Move/move_guard.lua
@@ -1,5 +1,5 @@
 function execute(score)
     for i, v in ipairs(targets) do
-        v.AddStatModifier(Stats.Speed, 5 * (power / 100) * score, 1)
+        v.AddStatModifier(Stats.Defence, 5 * (power / 100) * score, 1)
     end
 end
