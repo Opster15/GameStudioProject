f4d91675e042ca45f0ef0ea9aac1cbedba454437
Opster15 <60069488+Opster15@users.noreply.github.com>
2022-04-18 17:43:46 +0100

    Animations and walk script added

---
+++ Assets/Scripts/World/Villager_Move.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GSP
+{
+    public class Villager_Move : MonoBehaviour
+    {
+        public Transform[] positions;
+
+        public int counter;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            transform.position = positions[counter].position;
+            counter = 1;
+        }
+
+
+        private void Update()
+        {
+            if(transform.position == positions[counter].position)
+            {
+                counter++;
+                if(counter == positions.Length)
+                {
+                    counter = 0;
+                }
+            }
+
+            transform.LookAt(positions[counter].position);
+            transform.position = Vector3.MoveTowards(transform.position, positions[counter].position, 0.005f);
+        }
+    }
+}
