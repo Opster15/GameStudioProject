343027590d09df42e504f930412640a2de8e2c5a
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2022-05-01 19:17:04 +0100

    Added walk animations

--- Assets/Scripts/World/Battle/CharacterModel.cs
+++ Assets/Scripts/World/Battle/CharacterModel.cs
@@ -6,9 +6,10 @@ namespace GSP.World.Battle
 {
     public class CharacterModel : CharacterTargetedElement
     {
-        private static readonly int s_isDead = Animator.StringToHash("isDead");
-        private static readonly int s_isDamaged = Animator.StringToHash("isDamaged");
         private static readonly int s_isAttacking = Animator.StringToHash("isAttacking");
+        private static readonly int s_isWalking = Animator.StringToHash("isWalking");
+        private static readonly int s_isDamaged = Animator.StringToHash("isDamaged");
+        private static readonly int s_isDead = Animator.StringToHash("isDead");
         
         private GameObject m_model;
 
@@ -16,6 +17,8 @@ namespace GSP.World.Battle
 
         [SerializeField] private Vector3 m_selectedPosition;
         [SerializeField] private float m_moveTime;
+
+        private Sequence m_walkSequence;
 
         private void Start()
         {
@@ -46,7 +49,17 @@ namespace GSP.World.Battle
         private void OnSelected(bool _selected)
         {
             if (!m_target.BaseCharacter.WalkOnSelected) { return; }
-            transform.DOLocalMove(_selected ? m_selectedPosition : Vector3.zero, m_moveTime);
+            
+            if (m_walkSequence is { active: true })
+            {
+                m_walkSequence.Kill();
+            }
+
+            m_anim.SetBool(s_isWalking, true);
+
+            m_walkSequence = DOTween.Sequence();
+            m_walkSequence.Append(transform.DOLocalMove(_selected ? m_selectedPosition : Vector3.zero, m_moveTime));
+            m_walkSequence.AppendCallback(() => m_anim.SetBool(s_isWalking, false));
         }
 
         private void OnUsingMove()
