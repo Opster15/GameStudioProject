538b0541932f9d4b791de9c81a4950dac5232998
Kimera Royal <royalkimera01@gmail.com>
2022-05-03 16:32:28 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/Battle/Party/PlayerPartyHolder.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GSP
+{
+    public class PlayerPartyHolder : MonoBehaviour
+    {
+        void Awake()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
