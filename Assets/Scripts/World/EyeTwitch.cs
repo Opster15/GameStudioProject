0d2c8d1f5753dc7a658236010287c6606c2461c9
Kimera Royal <royalkimera01@gmail.com>
2022-05-04 12:02:46 +0100

    Made death door eyes look at random things

---
+++ Assets/Scripts/World/EyeTwitch.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+namespace GSP
+{
+    public class EyeTwitch : MonoBehaviour
+    {
+        [SerializeField] private Transform[] m_eyes;
+
+        [SerializeField] private Vector2 m_lookTimeDelay;
+        [SerializeField] private Vector2 m_lookTimeRange;
+        [SerializeField] private Vector2 m_lookTimeVariance;
+
+        [SerializeField] private Vector3 m_minTarget, m_maxTarget;
+
+        private void Start()
+        {
+            StartCoroutine(LookCycle());
+        }
+
+        private IEnumerator LookCycle()
+        {
+            yield return new WaitForSeconds(Random.Range(m_lookTimeDelay.x, m_lookTimeDelay.y));
+            LookAt(new Vector3(Random.Range(m_minTarget.x, m_maxTarget.x), Random.Range(m_minTarget.y, m_maxTarget.y), Random.Range(m_minTarget.z, m_maxTarget.z)), Random.Range(m_lookTimeRange.x, m_lookTimeRange.y));
+            StartCoroutine(LookCycle());
+        }
+
+        private void LookAt(Vector3 _target, float _time)
+        {
+            foreach(var eye in m_eyes)
+            {
+                eye.DOLookAt(_target, _time * Random.Range(m_lookTimeVariance.x, m_lookTimeVariance.y));
+            }
+        }
+    }
+}
